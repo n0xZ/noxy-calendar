356fb3599e7cbf00ef010ff55a1c7458955e4ad8
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-02 19:56:18 -0300

    chore: add convex provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './app.tsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { ConvexProvider, ConvexReactClient } from 'convex/react'
 import './tailwind.css'
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
+const convexClient = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL!)
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
-		<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-			<App />
-		</ClerkProvider>
-		,
+		<ConvexProvider client={convexClient}>
+			<ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+				<App />
+			</ClerkProvider>
+		</ConvexProvider>
 	</React.StrictMode>
 )
