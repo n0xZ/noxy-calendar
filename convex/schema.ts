54a01372436b8118c2ebfe6ef544718c5ba9b543
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-03 11:27:52 -0300

    chore: add reminderId field and searchIndex

--- convex/schema.ts
+++ convex/schema.ts
@@ -3,13 +3,19 @@ import { v } from 'convex/values'
 
 export default defineSchema({
 	reminders: defineTable({
+		reminderId: v.string(),
 		createdAt: v.string(),
 		description: v.string(),
 		expiringDate: v.string(),
 		title: v.string(),
 		userId: v.string(),
-	}).searchIndex('search_by_userId', {
-		searchField: 'userId',
-		filterFields: ['userId'],
-	}),
+	})
+		.searchIndex('search_by_userId', {
+			searchField: 'userId',
+			filterFields: ['userId'],
+		})
+		.searchIndex('search_by_id', {
+			searchField: 'reminderId',
+			filterFields: ['reminderId'],
+		}),
 })
