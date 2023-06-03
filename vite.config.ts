b4536dd90b94540a7f2ed6a1098f6ff93d1cf567
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-03 20:58:54 -0300

    chore: add new deps and setup millionjs

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from 'vite'
 
 import React from '@vitejs/plugin-react-swc'
 import AutoImport from 'unplugin-auto-import/vite'
+import Million from 'million/compiler'
 import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [React(), AutoImport({ imports: ['react', 'react-router-dom'] })],
+	plugins: [
+		React(),
+		Million.vite(),
+		AutoImport({ imports: ['react', 'react-router-dom'] }),
+	],
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, 'src'),
