3e1c30c06170b5499c1758dee565078558748d92
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-03 00:35:35 -0300

    chore: add autoimport plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 
 import React from '@vitejs/plugin-react-swc'
+import AutoImport from 'unplugin-auto-import/vite'
 import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [React()],
+	plugins: [React(), AutoImport({ imports: ['react', 'react-router-dom'] })],
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, 'src'),
