cb93fd526c27d51df074efd5e11b361e05932bc9
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-03 02:06:39 -0300

    chore: add ReminderSkeleton comp. to display it when loading

---
+++ src/components/reminders/skeleton.tsx
@@ -0,0 +1,135 @@
+import { Edit, ChevronRight } from 'lucide-react'
+import { Skeleton } from '../ui/skeleton'
+
+export default function ReminderSkeleton() {
+	return (
+		<section className="grid w-full gap-4">
+			{/* CardContainer */}
+			<article className="w-full h-48 bg-gray-100 border rounded-lg shadow-sm text-card-foreground">
+				{/* Card */}
+				<header className="flex flex-col space-y-1.5 p-6">
+					{/* CardHeader */}
+					<article>
+						{/* CardTitle */}
+						<aside className="flex flex-row items-center justify-between text-lg font-semibold leading-none tracking-tight">
+							<Skeleton className="h-6 bg-gray-300 w-36 animate-pulse" />
+							<Skeleton className="w-24 h-6 bg-gray-300 animate-pulse" />
+						</aside>
+					</article>
+				</header>
+				{/* CardContent */}
+				<article className="grid gap-5 p-6 pt-0">
+					<aside className="flex items-center justify-end p-4 space-x-5 border rounded-md">
+						<Edit className="w-6 h-6 hover:opacity-60" />
+						<ChevronRight className="w-6 h-6 hover:opacity-60" />
+					</aside>
+				</article>
+			</article>
+			{/* CardContainer */}
+			<article className="w-full h-48 bg-gray-100 border rounded-lg shadow-sm text-card-foreground">
+				{/* Card */}
+				<header className="flex flex-col space-y-1.5 p-6">
+					{/* CardHeader */}
+					<article>
+						{/* CardTitle */}
+						<aside className="flex flex-row items-center justify-between text-lg font-semibold leading-none tracking-tight">
+							<Skeleton className="h-6 bg-gray-300 w-36 animate-pulse" />
+							<Skeleton className="w-24 h-6 bg-gray-300 animate-pulse" />
+						</aside>
+					</article>
+				</header>
+				{/* CardContent */}
+				<article className="grid gap-5 p-6 pt-0">
+					<aside className="flex items-center justify-end p-4 space-x-5 border rounded-md">
+						<Edit className="w-6 h-6 hover:opacity-60" />
+						<ChevronRight className="w-6 h-6 hover:opacity-60" />
+					</aside>
+				</article>
+			</article>
+			{/* CardContainer */}
+			<article className="w-full h-48 bg-gray-100 border rounded-lg shadow-sm text-card-foreground">
+				{/* Card */}
+				<header className="flex flex-col space-y-1.5 p-6">
+					{/* CardHeader */}
+					<article>
+						{/* CardTitle */}
+						<aside className="flex flex-row items-center justify-between text-lg font-semibold leading-none tracking-tight">
+							<Skeleton className="h-6 bg-gray-300 w-36 animate-pulse" />
+							<Skeleton className="w-24 h-6 bg-gray-300 animate-pulse" />
+						</aside>
+					</article>
+				</header>
+				{/* CardContent */}
+				<article className="grid gap-5 p-6 pt-0">
+					<aside className="flex items-center justify-end p-4 space-x-5 border rounded-md">
+						<Edit className="w-6 h-6 hover:opacity-60" />
+						<ChevronRight className="w-6 h-6 hover:opacity-60" />
+					</aside>
+				</article>
+			</article>
+			{/* CardContainer */}
+			<article className="w-full h-48 bg-gray-100 border rounded-lg shadow-sm text-card-foreground">
+				{/* Card */}
+				<header className="flex flex-col space-y-1.5 p-6">
+					{/* CardHeader */}
+					<article>
+						{/* CardTitle */}
+						<aside className="flex flex-row items-center justify-between text-lg font-semibold leading-none tracking-tight">
+							<Skeleton className="h-6 bg-gray-300 w-36 animate-pulse" />
+							<Skeleton className="w-24 h-6 bg-gray-300 animate-pulse" />
+						</aside>
+					</article>
+				</header>
+				{/* CardContent */}
+				<article className="grid gap-5 p-6 pt-0">
+					<aside className="flex items-center justify-end p-4 space-x-5 border rounded-md">
+						<Edit className="w-6 h-6 hover:opacity-60" />
+						<ChevronRight className="w-6 h-6 hover:opacity-60" />
+					</aside>
+				</article>
+			</article>
+			{/* CardContainer */}
+			<article className="w-full h-48 bg-gray-100 border rounded-lg shadow-sm text-card-foreground">
+				{/* Card */}
+				<header className="flex flex-col space-y-1.5 p-6">
+					{/* CardHeader */}
+					<article>
+						{/* CardTitle */}
+						<aside className="flex flex-row items-center justify-between text-lg font-semibold leading-none tracking-tight">
+							<Skeleton className="h-6 bg-gray-300 w-36 animate-pulse" />
+							<Skeleton className="w-24 h-6 bg-gray-300 animate-pulse" />
+						</aside>
+					</article>
+				</header>
+				{/* CardContent */}
+				<article className="grid gap-5 p-6 pt-0">
+					<aside className="flex items-center justify-end p-4 space-x-5 border rounded-md">
+						<Edit className="w-6 h-6 hover:opacity-60" />
+						<ChevronRight className="w-6 h-6 hover:opacity-60" />
+					</aside>
+				</article>
+			</article>
+			{/* CardContainer */}
+			<article className="w-full h-48 bg-gray-100 border rounded-lg shadow-sm text-card-foreground">
+				{/* Card */}
+				<header className="flex flex-col space-y-1.5 p-6">
+					{/* CardHeader */}
+					<article>
+						{/* CardTitle */}
+						<aside className="flex flex-row items-center justify-between text-lg font-semibold leading-none tracking-tight">
+							<Skeleton className="h-6 bg-gray-300 w-36 animate-pulse" />
+							<Skeleton className="w-24 h-6 bg-gray-300 animate-pulse" />
+						</aside>
+					</article>
+				</header>
+				{/* CardContent */}
+				<article className="grid gap-5 p-6 pt-0">
+					<aside className="flex items-center justify-end p-4 space-x-5 border rounded-md">
+						<Edit className="w-6 h-6 hover:opacity-60" />
+						<ChevronRight className="w-6 h-6 hover:opacity-60" />
+					</aside>
+				</article>
+			</article>
+		</section>
+	)
+}
