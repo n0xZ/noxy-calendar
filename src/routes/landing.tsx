99cf091e6d153801875dd8f0afd4ea1118e21982
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-03 01:05:27 -0300

    chore: add underline utility class on header anchor

--- src/routes/landing.tsx
+++ src/routes/landing.tsx
@@ -5,18 +5,23 @@ export function Landing() {
 		<>
 			<header className="p-6 ">
 				<nav className="container flex flex-row items-center justify-between max-w-4xl mx-auto text-lg ">
-					<NavLink to="/" className="font-medium">
+					<NavLink to="/" className="font-medium ">
 						Noxy - calendar
 					</NavLink>
-					<NavLink to="/login">Empieza ya!</NavLink>
+					<NavLink to="/login" className="underline">
+						Empieza ya!
+					</NavLink>
 				</nav>
 			</header>
-			<main className="container flex flex-col justify-center h-screen max-w-4xl mx-auto space-y-5" >
+			<main className="container flex flex-col justify-center h-screen max-w-4xl mx-auto space-y-5">
 				<h2 className="text-5xl font-bold text-center text-primary">
 					Crear recordatorios nunca fue{' '}
 					<span className="text-emerald-500">tan facil.</span>
 				</h2>
-				<p className='text-center opacity-70'>En Noxy - calendar, podés definir tus recordatorios de todo tipo, y previo a su fecha máxima, recibirás una notificación del mísmo.</p>
+				<p className="text-center opacity-70">
+					En Noxy - calendar, podés definir tus recordatorios de todo tipo, y previo
+					a su fecha máxima, recibirás una notificación del mísmo.
+				</p>
 				<NavLink
 					to="/login"
 					className="self-center w-full max-w-xs p-3 mt-5 font-bold text-center text-white rounded-lg bg-emerald-500 hover:bg-emerald-500/60 "
