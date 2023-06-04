5355c8ffe36346503a2b319419840e2732c2c290
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-03 21:04:56 -0300

    remove millionjs due to its limitations related to UI comp.

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,11 @@ import { defineConfig } from 'vite'
 
 import React from '@vitejs/plugin-react-swc'
 import AutoImport from 'unplugin-auto-import/vite'
-import Million from 'million/compiler'
+
 import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		React(),
-		Million.vite(),
-		AutoImport({ imports: ['react', 'react-router-dom'] }),
-	],
+	plugins: [React(), AutoImport({ imports: ['react', 'react-router-dom'] })],
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, 'src'),
