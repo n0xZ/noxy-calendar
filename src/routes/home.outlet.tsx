dfe59f16a91a9775dba64882d03bf68d096d32de
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-05 13:59:14 -0300

    add menubar on home outlet

--- src/routes/home.outlet.tsx
+++ src/routes/home.outlet.tsx
@@ -1,26 +1,69 @@
-import { NavLink, Navigate, Outlet } from 'react-router-dom'
-import { SignedIn, SignedOut, UserButton, useUser } from '@clerk/clerk-react'
+import {
+	Menubar,
+	MenubarContent,
+	MenubarTrigger,
+	MenubarItem,
+	MenubarMenu,
+} from '@/components/ui/menubar'
+import { UserButton, useUser } from '@clerk/clerk-react'
 
+import { Authenticated, Unauthenticated, AuthLoading } from 'convex/react'
+import { ChevronUp, CalendarPlus } from 'lucide-react'
+
+function Avatar() {
+	return <div className="w-8 h-8 bg-gray-500 rounded-full animate-pulse"></div>
+}
+
+function HomeMenubar() {
+	return (
+		<Menubar className="space-x-6 bg-transparent border-0">
+			<MenubarMenu>
+				<MenubarTrigger className="flex flex-row items-center gap-1 ">
+					<ChevronUp />
+					<span>Recordatorio</span>
+				</MenubarTrigger>
+				<MenubarContent>
+					<MenubarItem>
+						<NavLink
+							to="/home/reminder/create"
+							className="flex flex-row items-center space-x-2"
+						>
+							<CalendarPlus />
+							<span>Crear nuevo recordatorio</span>
+						</NavLink>
+					</MenubarItem>
+				</MenubarContent>
+			</MenubarMenu>
+			<MenubarMenu>
+				<AuthLoading>
+					<Avatar />
+				</AuthLoading>
+				<Authenticated>
+					<UserButton afterSignOutUrl="/" />
+				</Authenticated>
+				<Unauthenticated>
+					<Navigate to="/" />
+				</Unauthenticated>
+			</MenubarMenu>
+		</Menubar>
+	)
+}
 export function HomeOutlet() {
 	const { isLoaded, user } = useUser()
 	return (
 		<>
-			<header className="p-6 border-b-2 border-primary-50 ">
-				<nav className="container flex flex-row items-center justify-between max-w-4xl mx-auto ">
-					<NavLink aria-disabled={!isLoaded} to="/home">
-						{isLoaded ? user?.username : 'Loading...'}
+			<header className="w-full p-5 text-base border-b-2 border-[#e9ecef]">
+				<nav className="container flex flex-row items-center justify-between max-w-4xl mx-auto font-bold opacity-80">
+					<NavLink aria-disabled={!isLoaded} to="/home" className="font-bold ">
+						{isLoaded ? user?.username : 'Cargando...'}
 					</NavLink>
-					<ul className="flex flex-row">
-						<SignedIn>
-							<UserButton afterSignOutUrl="/" />
-						</SignedIn>
-						<SignedOut>
-							<Navigate to="/" />
-						</SignedOut>
-					</ul>
+
+					<HomeMenubar />
 				</nav>
 			</header>
-			<Outlet />
+			<main className="container w-full h-full max-w-4xl mx-auto mt-5">
+				<Outlet />
+			</main>
 		</>
 	)
 }
