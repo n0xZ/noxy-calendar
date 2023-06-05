57155847dfbc3f903ec77d8495d8194c2108d5fa
Gonzalo Molina <o.gonzalo@hotmail.com>
2023-06-05 13:58:46 -0300

    update link bg color on hero

--- src/routes/landing.tsx
+++ src/routes/landing.tsx
@@ -4,11 +4,11 @@ export function Landing() {
 	return (
 		<>
 			<header className="p-6 ">
-				<nav className="container flex flex-row items-center justify-between max-w-4xl mx-auto text-lg ">
-					<NavLink to="/" className="font-medium ">
+				<nav className="container flex flex-row items-center justify-between max-w-4xl mx-auto text-lg  leading-7 [&:not(:first-child)] font-bold">
+					<NavLink to="/ " className="hover:opacity-50">
 						Noxy - calendar
 					</NavLink>
-					<NavLink to="/login" className="underline">
+					<NavLink to="/login" className="underline hover:opacity-50">
 						Empieza ya!
 					</NavLink>
 				</nav>
@@ -24,14 +24,14 @@ export function Landing() {
 				</p>
 				<NavLink
 					to="/login"
-					className="self-center w-full max-w-xs p-3 mt-5 font-bold text-center text-white rounded-lg bg-emerald-500 hover:bg-emerald-500/60 "
+					className="self-center w-full max-w-xs p-3 mt-5 font-bold text-center text-white rounded-lg bg-primary hover:bg-primary/80 "
 				>
 					Empieza ya a utilizar Noxy - calendar!
 				</NavLink>
 			</main>
-			<footer className="p-5 text-center">
+			<footer className="p-5 font-medium text-center">
 				<p>
-					© 2023, n0xZ. Powered by <span className="text-sky-600">React + Vite</span>{' '}
+					© 2023, n0xZ. Powered by <span className="text-sky-500">React + Vite</span>{' '}
 					💙
 				</p>
 			</footer>
